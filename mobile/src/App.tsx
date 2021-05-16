ac3a7bf19c8a90a3f5f99fcf55e5e7b946f8b6a9
Lucas Moreira <lucasmsea@outlook.com>
2021-05-16 02:06:22 -0300

    chore: add ignore warning logs to app, due to the fact that the app was having a `Sending "onAnimatedValueUpdate" with no listeners registered` warning

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,6 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react'
-import { View, StatusBar, SafeAreaView } from 'react-native'
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs(['Warning: ...']);
 import { NavigationContainer } from '@react-navigation/native'
 import Routes from './routes';
 import Toast from 'react-native-toast-message';
